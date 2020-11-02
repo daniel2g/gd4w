3c4e25fb9c065ec8ecca5200565de0b35c00f036
daniel2g <68967729+daniel2g@users.noreply.github.com>
2020-11-02 10:46:50 -0500

    11-2-1

--- js.js
+++ js.js
@@ -26,6 +26,23 @@
 
 $(document).ready(function(){
   
+
+  // $(".subhead").mouseover(function(){
+  //   // var rnum = Math.random(20)-10;
+  //   $(".subhead").animate({transform: "rotate(10deg)"});
+  // });
+  // // .mouseout(function(){
+  // //   $(".subhead").animate({transform: "rotateZ(0deg)"});
+  // // });
+ 
+  
+  
+
+
+
+
+
+
   $(".bigpic").click(function(){
     $(".bigpic").removeClass("animaterev");
     $(".readingdiv").removeClass("animatetxtrev");
