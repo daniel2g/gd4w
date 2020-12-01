90b9523f3868bf38acdd125420edf99b0cd6cdda
daniel2g <68967729+daniel2g@users.noreply.github.com>
2020-12-01 17:13:59 -0500

    final site dec 1

--- workers/js.js
+++ workers/js.js
@@ -1,19 +1,21 @@
 $(document).ready(function(){
+    $(this).scrollTop(0);
+    
     $("#field").focus();
     $("#field").select();
 
     
-    var fontSize = 30;
+    var fontSize = 28.6;
 
     document.getElementById("field").oninput = function(){
         
         var innerheight = document.getElementById("staticcontainer").clientHeight;
         
-        console.log('inner:' + innerheight);
+        // console.log('inner:' + innerheight);
         
         var outerheight = window.innerHeight;
 
-        console.log('outer:' + outerheight);
+        // console.log('outer:' + outerheight);
 
         if (innerheight > 0.85*outerheight) {
             fontSize--;
@@ -21,17 +23,39 @@ $(document).ready(function(){
             $(".h").css("font-size", fontSize/2 + "vh");
         };
 
-        if (fontSize < 30) {
+        if (fontSize < 28.6) {
             $("br").addClass("hide");
         };
         
-        console.log(fontSize);
-
-
+        // console.log(fontSize);
 
     };
 
+    $(window).on('scroll', function(){
+        var s = $(window).scrollTop(),
+            d = $(document).height(),
+            c = $(window).height();
+      
+        var scrollPercent = (s / (d - c)) * 100;
+        
 
+        // console.log(scrollPercent);
+
+        if (scrollPercent >= 80) {
+            $(".pub").removeClass("hidepub");
+          }
+        else { 
+            $(".pub").addClass("hidepub");
+        };
+
+
+
+      });
+
+      $(".button").click(function(){
+        document.location.reload();
+      });
+      
 
 
    
